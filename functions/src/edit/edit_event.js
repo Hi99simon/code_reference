44994dfd6699b848cfad33e80a11ea346cdea16b
SimonCC <41977267+Hi99simon@users.noreply.github.com>
2021-07-09 20:08:25 +0800

    Delete functions/src/edit directory

--- functions/src/edit/edit_event.js
+++
@@ -1,7 +0,0 @@
-const functions = require("firebase-functions");
-const admin = require("firebase-admin");
-const db = admin.firestore();
-
-exports.editEvent = functions.https.onCall(async (data, context) => {
-    console.log("edit_event.js")
-});
--- functions/src/edit/edit_post.js
+++
@@ -1,7 +0,0 @@
-const functions = require("firebase-functions");
-const admin = require("firebase-admin");
-const db = admin.firestore();
-
-exports.editPost = functions.https.onCall(async (data, context) => {
-    console.log("edit_post.js");
-});
